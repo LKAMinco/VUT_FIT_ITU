74552291eafc61b353fe8cb03a47349ff5dfd90b
Simona Češková <102659100+Shalatik@users.noreply.github.com>
2022-12-06 17:57:54 +0100

    Added json files for everything

--- user_profile.js
+++ user_profile.js
@@ -1,6 +1,6 @@
 function loadUser(){
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'src/users_list.json', true);
+    xhr.open('GET', 'src/users.json', true);
     document.getElementById("ree").innerHTML="Sup";
     xhr.onload = function(){
         var user = JSON.parse(this.responseText);
@@ -14,6 +14,28 @@ function loadUser(){
                 '<li>Adresa: ' + user.address + '</li>' +
                 '<li>Datum narození: ' + user.date_of_birth + '</li>' +
                 '</ul>';
+        document.getElementById('user').innerHTML = output;
+    }
+
+    xhr.send();
+}
+
+function loadTickets(){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'src/users_list.json', true);
+    document.getElementById("ree").innerHTML="Sup";
+    xhr.onload = function(){
+        var user = JSON.parse(this.responseText);
+
+        var output = '';
+
+        output += '<ul>' + '<b>aaaa</b>' +
+            '<li>Jméno: ' + user.name + '</li>' +
+            '<li>Email: ' + user.email + '</li>' +
+            '<li>Přijmení: ' + user.surname + '</li>' +
+            '<li>Adresa: ' + user.address + '</li>' +
+            '<li>Datum narození: ' + user.date_of_birth + '</li>' +
+            '</ul>';
         document.getElementById('user').innerHTML = output;
     }
 
