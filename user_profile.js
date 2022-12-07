67c5423f2d5c26cd4cfeaba23a607423c3e7ad9d
Simona Češková <102659100+Shalatik@users.noreply.github.com>
2022-12-07 18:29:06 +0100

    test

--- user_profile.js
+++ user_profile.js
@@ -1,3 +1,5 @@
+
+
 function loadUser() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'src/users.json', true);
@@ -73,41 +75,3 @@ function editProfile() {
     xhr1.send();
 }
 
-// ************************  SIGNUP POPUP ************************
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
-
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
-
-overlay.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.modal.active')
-    modals.forEach(modal => {
-        closeModal(modal)
-    })
-})
-
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
-
-function openModal(modal) {
-    if (modal == null) return
-    modal.classList.add('active')
-    overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-    if (modal == null) return
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
-}
-// ************************  SIGNUP POPUP ************************
