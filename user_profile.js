1aefe5f28b265de9f82334035581e1120c145fb8
Simona Češková <102659100+Shalatik@users.noreply.github.com>
2022-12-08 14:30:15 +0100

    filters except date filter done

--- user_profile.js
+++ user_profile.js
@@ -44,16 +44,14 @@ function loadTickets() {
         var ticker_list_filtered = ticker_list.filter((ticket) => ticket.user_email === 'ree@email.com');
         var output = '';
         document.getElementById('ticket').innerHTML = ticker_list_filtered;
-        console.log(ticker_list_filtered);
         for (var i in ticker_list_filtered) {
-            output += '<tr>' +
+            output += '<tr class="ticket_body"><ul><li>' +
                 '<td>' + ticker_list_filtered[i].title + '</td>' +
                 '<td>' + ticker_list_filtered[i].address + '</td>' +
-                '<td>' + ticker_list_filtered[i].description + '</td>' +
                 '<td>' + ticker_list_filtered[i].category + '</td>' +
                 '<td>' + ticker_list_filtered[i].status + '</td>' +
-                '<div class="ticket_img_content"><td><img src="' + ticker_list_filtered[i].image_path + '" class="ticket_img_small" alt="Ticket picture"></td></div>' +
-                '</tr>';
+                '<td><img src="' + ticker_list_filtered[i].image_path + '" class="ticket_img_small" alt="Ticket picture"></td></li>' +
+                '<li><td class="ticket_description" colspan=5>' + ticker_list_filtered[i].description + '</td></li></ul></tr>';
             document.getElementById('ticket').innerHTML = output;
         }
     }
@@ -72,23 +70,47 @@ function editProfile() {
 
 /* **********************************DROPDOWN MENU **************************** */
 var searchFilter = () => {
-    let selectedColor = document.getElementById("filterByColor").value;
-    const input = document.querySelector(".form-control");
-    let textBox = input.value;
-    const cards = document.getElementsByClassName("col");
-    for (let i = 0; i < cards.length; i++) {
-        let title = cards[i].querySelector(".card-body");
-        if ((cards[i].classList.contains(selectedColor) || selectedColor == "") && title.innerText.toLowerCase().indexOf(textBox.toLowerCase()) > -1) {
-            cards[i].classList.remove("d-none");
-        } else {
-            cards[i].classList.add("d-none");
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'src/tickets.json', true);
+    xhr.onload = function () {
+        var ticker_list = JSON.parse(this.responseText);
+        var ticker_list_filtered = ticker_list.filter((ticket) => ticket.user_email === 'ree@email.com');
+        let status = document.getElementById("filter1").value;
+        let category = document.getElementById("filter2").value;
+        let date = document.getElementById("filter3").value;
+        if(status != "all")
+        {
+            ticker_list_filtered = ticker_list_filtered.filter((ticket) => ticket.status === status);
+        }
+        if(category != "all")
+        {
+            ticker_list_filtered = ticker_list_filtered.filter((ticket) => ticket.category === category);
+        }
+        ticker_list_filtered = ticker_list_filtered.sort((a, b) => b.date - a.date)
+
+        var output = '';
+        document.getElementById('ticket').innerHTML = ticker_list_filtered;
+        console.log(ticker_list_filtered);
+        for (var i in ticker_list_filtered) {
+            output += '<tr class="ticket_body">' +
+                '<td>' + ticker_list_filtered[i].title + '</td>' +
+                '<td>' + ticker_list_filtered[i].date + '</td>' +
+                '<td>' + ticker_list_filtered[i].address + '</td>' +
+                '<td>' + ticker_list_filtered[i].category + '</td>' +
+                '<td>' + ticker_list_filtered[i].status + '</td>' +
+                '<td><img src="' + ticker_list_filtered[i].image_path + '" class="ticket_img_small" alt="Ticket picture"></td>' +
+                '</tr>';
+            document.getElementById('ticket').innerHTML = output;
         }
     }
+    xhr.send();
 }
+
 /* **********************************DROPDOWN MENU **************************** */
 /* **********************************MODAL IMG **************************** */
+/*
 // all images inside the image modal content class
-const lightboxImages = document.querySelectorAll('.ticket_img_content img');
+const lightboxImages = document.querySelectorAll('.ticket_img_small');
 
 // dynamically selects all elements inside modal popup
 const modalElement = element =>
@@ -118,5 +140,5 @@ lightboxImages.forEach(img => {
         modalElement('img').src = img.src;
     });
 });
-
+*/
 /* **********************************MODAL IMG **************************** */
