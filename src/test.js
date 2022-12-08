f14fd1642f695ef44e0935a68c97e94db0ad8f18
Simona Češková <102659100+Shalatik@users.noreply.github.com>
2022-12-08 17:53:55 +0100

    icons

--- src/test.js
+++ src/test.js
@@ -1,38 +1,90 @@
-// ************************  SIGNUP POPUP ************************
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+function reportFirst(){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'src/users.json', true);
+    xhr.onload = function () {
+        var output = '';
 
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
+        output += '<section id="services">\n' +
+            '        <div class="content">\n' +
+            '            <h3>1/5 Vyberte kategorii hlášení:</h3>\n' +
+            '            <div>\n' +
+            '                <div class="services-column">\n' +
+            '                    <div class="services-card">\n' +
+            '                        <div>\n' +
+            '                            <img src="pictures/roads.svg" alt=".">\n' +
+            '                            <img src="pictures/roads.svg" alt=".">\n' +
+            '                        </div>\n' +
+            '                        <h4>Problémy na silnicích</h4>\n' +
+            '                    </div>\n' +
+            '                </div>\n' +
+            '                <div class="services-column">\n' +
+            '                    <div class="services-card">\n' +
+            '                        <div>\n' +
+            '                            <img src="pictures/bench.svg" alt=".">\n' +
+            '                            <img src="pictures/bench.svg" alt=".">\n' +
+            '                        </div>\n' +
+            '                        <h4>Lavičky</h4>\n' +
+            '                    </div>\n' +
+            '                </div>\n' +
+            '                <div class="services-column">\n' +
+            '                    <div class="services-card">\n' +
+            '                        <div>\n' +
+            '                            <img src="pictures/dump.svg" alt=".">\n' +
+            '                            <img src="pictures/dump.svg" alt=".">\n' +
+            '                        </div>\n' +
+            '                        <h4>Ilegální skládky</h4>\n' +
+            '                    </div>\n' +
+            '                </div>\n' +
+            '                <div class="services-column">\n' +
+            '                    <div class="services-card">\n' +
+            '                        <div>\n' +
+            '                            <img src="pictures/park.svg" alt=".">\n' +
+            '                            <img src="pictures/park.svg" alt=".">\n' +
+            '                        </div>\n' +
+            '                        <h4>Parky a zeleň</h4>\n' +
+            '                    </div>\n' +
+            '                </div>\n' +
+            '                <div class="services-column">\n' +
+            '                    <div class="services-card">\n' +
+            '                        <div>\n' +
+            '                            <img src="pictures/car.svg" alt=".">\n' +
+            '                            <img src="pictures/car.svg" alt=".">\n' +
+            '                        </div>\n' +
+            '                        <h4>Opuštěná vozidla</h4>\n' +
+            '                    </div>\n' +
+            '                </div>\n' +
+            '                <div class="services-column">\n' +
+            '                    <div class="services-card">\n' +
+            '                        <div>\n' +
+            '                            <img src="pictures/playground.svg" alt=".">\n' +
+            '                            <img src="pictures/playground.svg" alt=".">\n' +
+            '                        </div>\n' +
+            '                        <h4>Dětská hřiště</h4>\n' +
+            '                    </div>\n' +
+            '                </div>\n' +
+            '                <div class="services-column">\n' +
+            '                    <div class="services-card">\n' +
+            '                        <div>\n' +
+            '                            <img src="pictures/vandalism.svg" alt=".">\n' +
+            '                            <img src="pictures/vandalism.svg" alt=".">\n' +
+            '                        </div>\n' +
+            '                        <h4>Vandalismus</h4>\n' +
+            '                    </div>\n' +
+            '                </div>\n' +
+            '                <div class="services-column">\n' +
+            '                    <div class="services-card">\n' +
+            '                        <div>\n' +
+            '                            <img src="pictures/others.svg" alt=".">\n' +
+            '                            <img src="pictures/others.svg" alt=".">\n' +
+            '                        </div>\n' +
+            '                        <h4>Ostatní</h4>\n' +
+            '                    </div>\n' +
+            '                </div>\n' +
+            '            </div>\n' +
+            '        </div>\n' +
+            '    </section>';
+        document.getElementById('report_first_part').innerHTML = output;
+    }
 
-overlay.addEventListener('click', () => {
-    const modals = document.querySelectorAll('.modal.active')
-    modals.forEach(modal => {
-        closeModal(modal)
-    })
-})
-
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
-
-function openModal(modal) {
-    if (modal == null) return
-    modal.classList.add('active')
-    overlay.classList.add('active')
+    xhr.send();
 }
-
-function closeModal(modal) {
-    if (modal == null) return
-    modal.classList.remove('active')
-    overlay.classList.remove('active')
-}
-// ************************  SIGNUP POPUP ************************
