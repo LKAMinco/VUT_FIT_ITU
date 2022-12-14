5d46da1e54bc0f23db8d4386833faa7c84dffbd1
JuliusJx <jakubjsmykal@gmail.com>
2022-12-14 19:57:18 +0100

    Fix error in user profile page

--- src/user_profile.js
+++ src/user_profile.js
@@ -13,6 +13,7 @@ function loadUser() {
         output += '<li><h3>' + active_user.email + '</h3></li>' +
             '<li><h3>' + active_user.date_of_birth + '</h3></li>';
         document.getElementById('user').innerHTML = output;
+        loadTickets();
     }
 
     xhr.send();
